276d43b5e8955c05023c0213ad5183f0b9a3973e
trungnob <trungnob@5ba9accf-8f55-0410-b14e-91f8bd08dece>
2008-08-29 02:24:21 +0000

    First import

---
+++ trunk/buyLotsOfFruits.py
@@ -0,0 +1,11 @@
+fruitPrices = {'apples':2.00, 'oranges': 1.50, 'pears': 1.75, 
+              'limes':0.75,'strawberries':1.00}
+
+def buyLotsOfFruit(orderList):
+    """
+        orderList: List of (fruit, numPounds) tuples
+            
+    Returns cost of order
+    """ 
+   #TODO: implement here
+   return None 
---
+++ trunk/shopSmart.py
@@ -0,0 +1,10 @@
+import shop
+
+def shopSmart(orderList, fruitShops):
+    """
+        orderList: List of (fruit, numPound) tuples
+        fruitShops: List of FruitShops
+    """    
+    #TODO: your implementation here
+    return None
+    
