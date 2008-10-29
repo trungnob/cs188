ee2eaf4a43ce0851affc8bf5495e575eb287bb85
bernardw <bernardw@5ba9accf-8f55-0410-b14e-91f8bd08dece>
2008-10-29 09:10:29 +0000

    testing

--- trunk/src/proj4/staticInferenceModule.py
+++ trunk/src/proj4/staticInferenceModule.py
@@ -34,14 +34,38 @@ class StaticInferenceModule:
     
     Note that the observations are given as a dictionary.
     """
-    util.raiseNotDefined()
-    
+    ghost_tups_obs = Counter()
+    ghost_tups = self.game.getInitialDistribution()
+    for ghost_tup in self.game.getGhostTuples():
+        p = ghost_tups.getCount(ghost_tup)
+        for obs in observations.items():
+            sensor, reading = obs
+            Reading_given_ghost_tups = self.game.getGhostTupleDistributionGivenPreviousGhostTuple(ghost_tup, sensor)
+            reading_given_ghost_tups = Reading_given_ghost_tups.getCount(reading)
+            p *= reading_given_ghost_tups
+        ghost_tups_obs.setCount(ghost_tup, p)
+    ghost_tups_obs = normalize(ghost_tups_obs)
+    return ghost_tups_obs
+        
   def getReadingDistributionGivenObservations(self, observations, newLocation):
     """
     Compute the distribution over readings for the new location, given the
     current observations (given as a dictionary).
     """
-    util.raiseNotDefined()
+    old_reading_new_loc = self.fetch(newLocation, observations)
+    new_reading_obs = Counter()
+    Ghost_tups_given_obs = self.getGhostTupleDistributionGivenObservations(observations)
+    for ghost_tup, ghost_tups_given_obs in Ghost_tups_given_obs.items():
+        Reading_given_tup = self.game.getReadingDistributionGivenGhostTuple(ghost_tup, newLocation)
+        for reading in Readings.getReadings():
+            reading_given_tup = Reading_given_tup.getCount(reading)
+            if old_reading_new_loc != None:
+                obs_given_ghost = 0.0
+                if old_reading_new_loc == reading:
+                    obs_given_ghost = 1.0
+            new_reading_obs.incrementCount(reading, ghost_tups_given_obs * reading_given_tup)
+    new_reading_given_obs = normalize(new_reading_obs)
+    return new_reading_given_obs                 
 
 class ExactStaticInferenceModule(StaticInferenceModule):
   """
